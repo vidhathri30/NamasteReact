571ae3303c3c386cefe75a5a74c17ff1da09f55b
vidhathri30 <vidhathri30@gmail.com>
2024-05-15 21:47:40 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"hellooh1"),
+        React.createElement("h2",{},"hellooh2")
+    ]),
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"hellooh1"),React.createElement("h2",{},"hellooh2")
+    ]),
+
+]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
