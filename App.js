af323f8977a518fa761b00f3a7e9d9fb52a61075
vidhathri30 <vidhathri30@gmail.com>
2024-05-16 17:22:02 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading=React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child1"},[
         React.createElement("h1",{},"hellooh1"),
